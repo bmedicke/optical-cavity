515a4a5490aee1ad97350b5762fb25457047c4a3
Benjamin Medicke <bmedicke@gmail.com>
2022-06-10 11:41:15 +0200

    add wavelength2rgb() function

--- simulator/src/Visualizations.js
+++ simulator/src/Visualizations.js
@@ -1,6 +1,34 @@
 import styles from './Visualizations.module.scss'
 import { useEffect, useRef } from 'react'
 
+const wavelength2rgb = (wl) => {
+  console.log(wl)
+  var color
+  const s = wl < 420 ? 0.3 + (0.7 * (wl - 380.0)) / (420.0 - 380.0) : 1.0
+  if (wl >= 380 && wl < 440) {
+    color = [(s * -1 * (wl - 440)) / (440 - 380), 0, s]
+  } else if (wl >= 440 && wl < 490) {
+    color = [0, (wl - 440) / (490 - 440), 1]
+  } else if (wl >= 490 && wl < 510) {
+    color = [0, 1, (-1 * (wl - 510)) / (510 - 490)]
+  } else if (wl >= 510 && wl < 580) {
+    color = [(wl - 510) / (580 - 510), 1.0, 0.0]
+  } else if (wl >= 580 && wl < 645) {
+    color = [1, (-1 * (wl - 645)) / (645 - 580), 0]
+  } else if (wl >= 645 && wl < 750) {
+    color = [0.3 + (0.7 * (750 - wl)) / (750 - 700), 0, 0]
+  } else {
+    color = [1, 1, 1]
+  }
+  const gamma = 1
+  const [r, g, b] = [color[0] * gamma, color[1] * gamma, color[2] * gamma]
+  return {
+    r: Math.pow(r, gamma) * 255,
+    g: Math.pow(g, gamma) * 255,
+    b: Math.pow(b, gamma) * 255,
+  }
+}
+
 function draw_sine(canvas, context, scale, phaseshift = 0) {
   context.beginPath()
 
