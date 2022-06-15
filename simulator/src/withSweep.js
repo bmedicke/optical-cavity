a623dc1f2ec369294320de660128b9dc09acb4e6
Benjamin Medicke <bmedicke@gmail.com>
2022-06-15 08:57:02 +0200

    remove log

--- simulator/src/withSweep.js
+++ simulator/src/withSweep.js
@@ -3,18 +3,25 @@ import React, { memo, useEffect, useState } from 'react'
 
 //props: minimum, maximum, setter, delay, step}
 export const withSweep = (BaseComponent) => (props) => {
+  const [isIncreasing, setIsIncreasing] = useState(true)
 
-    const [isIncreasing, setIsIncreasing] = useState(true)    
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (props.isActive) {
+        props.setter((x) => parseInt(x) + 1)
+      }
+    }, 500)
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-           if(props.isActive) {props.setter(x => (parseInt(x)+1))}
-        }, 500)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
 
-        return(() => {clearInterval(interval); console.log("bye bye")})
-    },[])
-
-  return <BaseComponent {...props} isResult isIncreasing={isIncreasing}>
-    <button onMouseUp={() => props.setIsActive(x => !x)}>{props.isActive ? "Stop Sweep" : "Start Sweep"}</button>
+  return (
+    <BaseComponent {...props} isResult isIncreasing={isIncreasing}>
+      <button onMouseUp={() => props.setIsActive((x) => !x)}>
+        {props.isActive ? 'Stop Sweep' : 'Start Sweep'}
+      </button>
     </BaseComponent>
+  )
 }
