8b52ab5454cd6a9aa113bf285aa206cada1783b4
Benjamin Medicke <bmedicke@gmail.com>
2022-06-19 10:07:39 +0200

    add useInterval()

--- simulator/src/utilities.js
+++ simulator/src/utilities.js
@@ -5,4 +5,21 @@ const rad2deg = (rad) => {
   return rad * (180 / Math.PI)
 }
 
-export { deg2rad, rad2deg }
+function useInterval(callback, delay) {
+  // https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+  const savedCallback = useRef()
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+  useEffect(() => {
+    function tick() {
+      savedCallback.current()
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay)
+      return () => clearInterval(id)
+    }
+  }, [delay])
+}
+
+export { deg2rad, rad2deg, useInterval }
