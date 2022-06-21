f26a9b217c15b9dd53fe60fa6511c656bd59c506
Benjamin Medicke <bmedicke@gmail.com>
2022-06-21 13:35:32 +0200

    be explicit about mqtt connection protocol

--- simulator/src/App.js
+++ simulator/src/App.js
@@ -108,7 +108,7 @@ function App() {
   // strict mode renders App twice (dev mode only):
   // https://reactjs.org/docs/strict-mode.html
   useEffect(() => {
-    const URL = 'mqtt://test.mosquitto.org:8080'
+    const URL = 'wss://test.mosquitto.org:8081'
     const client = mqtt.connect(URL)
     const topic = 'optical-cavity-simulator'
 
