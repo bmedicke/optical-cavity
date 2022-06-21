233f4f36a4b1af685e717ba6e5161fbf2e90cbed
Niko <benediktnikolai@gmail.com>
2022-06-21 02:21:53 +0200

    define states

--- simulator/src/Simulator/scenes/CavityScene.js
+++ simulator/src/Simulator/scenes/CavityScene.js
@@ -7,6 +7,8 @@ import { CavityContext } from '../ctx/CavityContext'
 import { convertDegToRad } from '../helpers'
 
 const CavityScene = () => {
+  const [isResonant, setIsResonant] = useState(true) //to be done
+  const { caviLength } = useContext(CavityContext) // test out countext state
   return (
   )
 }
