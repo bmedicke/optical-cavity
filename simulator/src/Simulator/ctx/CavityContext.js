046517dd78fb2a4812f3c07b15b364677c388b0a
Niko <benediktnikolai@gmail.com>
2022-06-21 02:18:40 +0200

    add cavity provider component

--- simulator/src/Simulator/ctx/CavityContext.js
+++ simulator/src/Simulator/ctx/CavityContext.js
@@ -1,2 +1,18 @@
 import { createContext, useMemo, useState } from 'react'
 
+export const CavityContext = createContext({})
+
+export const CavityProvider = (props) => {
+  const [caviLength, setCaviLength] = useState(150.0)
+
+  const value = useMemo(
+    () => ({
+      caviLength,
+      setCaviLength,
+    }),
+    [caviLength]
+  )
+  return <CavityContext.Provider value={value} {...props} />
+}
+
+export default CavityProvider
