9fc1576ba3676d88e3e4c9ce832a1a40d2c81e96
Niko <benediktnikolai@gmail.com>
2022-06-21 04:02:51 +0200

    add coloring from context

--- simulator/src/Simulator/scenes/CavityScene.js
+++ simulator/src/Simulator/scenes/CavityScene.js
@@ -1,19 +1,26 @@
 //const [cavityLength, setCavityLength] = useState(length)
-import { useContext, useState } from 'react'
+import { useContext, useState, useEffect } from 'react'
+import { rgb2string } from '../../Visualizations.js'
 import CameraController from '../components/CameraController'
 import Mirror from '../components/Mirror'
 import Ray from '../components/Ray'
 import { CavityContext } from '../ctx/CavityContext'
 import { convertDegToRad } from '../helpers'
 
-// TO BE DONE : ray length in the middle wrong!!
+// TO BE DONE : COLORING NOT YET FINISHED
 const CavityScene = () => {
   const [isResonant, setIsResonant] = useState(true) //to be done
-  const { caviLength } = useContext(CavityContext) // test out countext state
+  const { caviLength, setCaviLength } = useContext(CavityContext) // test out countext state
+  const { wavelengthColor, setWavelengthColor } = useContext(CavityContext) // test out countext state
 
   let POS_ITM = 0
   let POS_ETM = caviLength / 100
   const rayThickness = 0.05
+
+  useEffect(() => {
+    console.log(wavelengthColor)
+  }, [wavelengthColor])
+
   return (
     <>
       <CameraController />
@@ -24,21 +31,21 @@ const CavityScene = () => {
       <Ray
         rayThickness={rayThickness}
         length={10}
-        color={'#ff6c65'}
+        color={`${rgb2string(wavelengthColor, 0.1)}`}
         position={[-10 / 2, 0, 0]} //fixed
         rotation={[0, 0, convertDegToRad(90)]}
       />
       <Ray
         rayThickness={rayThickness}
         length={caviLength / 100}
-        color={isResonant ? 'red' : '#ff6c65'}
+        color={isResonant ? 'red' : `${rgb2string(wavelengthColor, 0.1)}`}
         position={[caviLength / 100 / 2, 0, 0]}
         rotation={[0, 0, convertDegToRad(90)]}
       />
       <Ray
         rayThickness={rayThickness}
         length={100 - caviLength / 100}
-        color={'#ff6c65'}
+        color={`${rgb2string(wavelengthColor, 0.1)}`}
         position={[caviLength / 100 + (100 - caviLength / 100) / 2, 0, 0]}
         rotation={[0, 0, convertDegToRad(90)]}
       />
