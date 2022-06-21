3e131395eefd3846322d694dea57ecdd0310cdf1
Niko <benediktnikolai@gmail.com>
2022-06-21 02:19:19 +0200

    necessary imports

---
+++ simulator/src/Simulator/scenes/CavityScene.js
@@ -0,0 +1,7 @@
+//const [cavityLength, setCavityLength] = useState(length)
+import { useContext, useState } from 'react'
+import CameraController from '../components/CameraController'
+import Mirror from '../components/Mirror'
+import Ray from '../components/Ray'
+import { CavityContext } from '../ctx/CavityContext'
+import { convertDegToRad } from '../helpers'
