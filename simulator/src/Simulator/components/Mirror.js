4e622111ab0520bd133c7abbad22331cd0508ef8
Niko <benediktnikolai@gmail.com>
2022-06-21 10:25:16 +0200

    use deg2rad instead

--- simulator/src/Simulator/components/Mirror.js
+++ simulator/src/Simulator/components/Mirror.js
@@ -1,12 +1,12 @@
 import { useRef } from 'react'
 import { CylinderBufferGeometry } from 'three'
-import { convertDegToRad } from '../helpers.js'
+import { deg2rad } from '../../utilities.js'
 
 const Mirror = (props) => {
   const mesh = useRef()
   const cylinderBufferGeometry = new CylinderBufferGeometry()
   return (
-    <mesh {...props} rotation={[0, 0, convertDegToRad(90)]} ref={mesh}>
+    <mesh {...props} rotation={[0, 0, deg2rad(90)]} ref={mesh}>
       <cylinderBufferGeometry attach="geometry" args={[1, 1, 0.01, 50]} />
       <meshStandardMaterial attach="material" color={'#aaa'} />
     </mesh>
