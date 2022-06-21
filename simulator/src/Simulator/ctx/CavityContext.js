77a5b4156f2ebac83f233c897ea10a616111c810
Niko <benediktnikolai@gmail.com>
2022-06-21 02:38:39 +0200

    add state for setting 3d mode

--- simulator/src/Simulator/ctx/CavityContext.js
+++ simulator/src/Simulator/ctx/CavityContext.js
@@ -4,11 +4,14 @@ export const CavityContext = createContext({})
 
 export const CavityProvider = (props) => {
   const [caviLength, setCaviLength] = useState(150.0)
+  const [is3D, setIs3D] = useState(false)
 
   const value = useMemo(
     () => ({
       caviLength,
       setCaviLength,
+      is3D,
+      setIs3D,
     }),
     [caviLength]
   )
