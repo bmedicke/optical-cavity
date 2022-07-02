5285da256ed3501e3ec5fc861c0e11682d4c56d9
Benjamin Medicke <bmedicke@gmail.com>
2022-07-02 09:39:07 +0200

    update service worker
    Co-authored-by: Nikolai Benedikt <benediktnikolai@gmail.com>

--- simulator/public/serviceWorker.js
+++ simulator/public/serviceWorker.js
@@ -1,8 +1,6 @@
-let CACHE_NAME = 'cavity-simulator'
-let urlsToCache = ['/', '/completed']
-
-// Install a service worker
-self.addEventListener('install', (event) => {
+let CACHE_NAME = 'my-site-cache-v1'
+const urlsToCache = ['/', '/index.html']
+self.addEventListener('install', function (event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
@@ -12,31 +10,13 @@ self.addEventListener('install', (event) => {
   )
 })
 
-// Cache and return requests
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
-      // Cache hit - return response
       if (response) {
         return response
       }
       return fetch(event.request)
-    })
-  )
-})
-
-// Update a service worker
-self.addEventListener('activate', (event) => {
-  let cacheWhitelist = ['cavity-simulator']
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName)
-          }
-        })
-      )
     })
   )
 })
